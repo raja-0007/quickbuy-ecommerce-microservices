5a681c5c5815ef5b4a87ca60cab1b1efc8a226f4
raja-0007 <rajadaragani07@gmail.com>
2026-03-02 10:47:28 +0530

    removed debut console logs

--- services/product-service/src/product_handlers/controller.js
+++ services/product-service/src/product_handlers/controller.js
@@ -5,7 +5,6 @@ const productServices = require('./service')
 const getHomeDeals = async(req, res) =>{
     try{
         const deals = await productServices.getHomeDeals();
-        console.log('returning deals', deals);
         res.status(200).json({ deals });
     }catch(err){
         res.status(500).json({ error: err.message });
@@ -21,10 +20,10 @@ const getCategories = async(req, res) =>{
     }
 }
 const getAllProducts = async(req, res) =>{
-    console.log("Get all products");
+    // console.log("Get all products");
     try{
         const {limit, page, searchQuery} = req.query;
-        console.log("Limit:", limit, "Page:", page, "Search Query:", searchQuery);
+        // console.log("Limit:", limit, "Page:", page, "Search Query:", searchQuery);
         const response = await productServices.getAllProducts(limit, page, searchQuery);
         res.status(200).json({ products: response });
     }catch(err){
@@ -35,7 +34,7 @@ const searchProducts = async(req, res) =>{
     console.log("Get all products");
     try{
         const {limit, page, searchQuery} = req.query;
-        console.log("Limit:", limit, "Page:", page, "Search Query:", searchQuery);
+        // console.log("Limit:", limit, "Page:", page, "Search Query:", searchQuery);
         const response = await productServices.searchProducts(limit, page, searchQuery);
         res.status(200).json({ products: response });
     }catch(err){
@@ -43,7 +42,7 @@ const searchProducts = async(req, res) =>{
     }
 }
 const getProductById = async(req, res) =>{
-    console.log("Get all products");
+    // console.log("Get all products");
     // res.status(200).json({ product: {id:1} });
     try{
         const { id } = req.params;
@@ -57,20 +56,20 @@ const getProductById = async(req, res) =>{
 
 const addProduct = async(req, res) =>{
     const { name, price, description } = req.body;
-    console.log("Add product:", name, price, description);
+    // console.log("Add product:", name, price, description);
     res.status(201).json({ message: 'Product added successfully' });
 }
 
 const updateProduct = async(req, res) =>{
     const { id } = req.params;
     const { name, price, description } = req.body;
-    console.log("Update product:", id, name, price, description);
+    // console.log("Update product:", id, name, price, description);
     res.status(200).json({ message: 'Product updated successfully' });
 }
 
 const deleteProduct = async(req, res) =>{
     const { id } = req.params;
-    console.log("Delete product:", id);
+    // console.log("Delete product:", id);
     res.status(200).json({ message: 'Product deleted successfully' });
 }
 
